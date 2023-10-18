09da12801d4f279a3c0e94eb0b9d4162145b8241
Daniel <daniel.lopez46@alu.uclm.es>
2023-10-18 15:52:15 +0200

    empiece de interfaz, formulario de dar de alta titulo (solo nombre y isbn) pero no lo guarda en BBDD (mirar formAltaTitulo)

--- Library/src/main/java/com/library/Library/controller/GestorTitulos.java
+++ Library/src/main/java/com/library/Library/controller/GestorTitulos.java
@@ -8,24 +8,32 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.library.Library.entity.Titulo;
 import com.library.Library.service.IServiceTitulo;
 
 @Controller
 @RequestMapping("/")
 public class GestorTitulos {
 	
-	//@Autowired
-	//private IServiceTitulo tituloService; //Uso interfaz de servicios de titulo
+	@Autowired
+	private IServiceTitulo tituloService; //Uso interfaz de servicios de titulo
 	
-	@GetMapping("/") //endpoint que estamos mapeando
-	public String crearTitulo(Model model) {
+	
+	@GetMapping("/altaTitulo") //endpoint que estamos mapeando
+	public String mostrarForm(Model model) {
 		
-		//Titulo titulo = new Titulo();
-		//model.addAttribute("titulo",titulo);
-		//mas model atributes en funcion de lo que necesitemos ver en el html
+		Titulo titulo = new Titulo();
+		model.addAttribute("titulo",titulo);
+				
 				
 		return "views/titulos/formAltaTitulo"; //RUTA A ARCHIVO .HTML DONDE ESTE EL FORMULARIO DE DAR DE ALTA UN TITULO
 	}
 
+	@PostMapping("/saved")
+	public String altaTitulo(@ModelAttribute Titulo titulo) {
+		
+		tituloService.altaTitulo(titulo);
+		return "redirect:index";
+	}
 
 }
