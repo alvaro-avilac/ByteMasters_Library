a2b162a4bf48a5f6ec583333468d5ad0338ed0e6
VictorLG89 <victor.lay@alu.uclm.es>
2023-10-23 20:46:35 +0200

    DAO usuario hecho

--- Library/src/main/java/com/library/Library/repository/UsuarioDAO.java
+++ Library/src/main/java/com/library/Library/repository/UsuarioDAO.java
@@ -3,9 +3,9 @@ package com.library.Library.repository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import com.library.Library.entity.Titulo;
+import com.library.Library.entity.Usuario;
 
 @Repository
-public interface UsuarioDAO extends CrudRepository<Titulo, Long>{
+public interface UsuarioDAO extends CrudRepository<Usuario, Long>{
 
 }
