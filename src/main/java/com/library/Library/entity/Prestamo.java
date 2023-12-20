fb51594ca3c43659530849a130498a48e83a0f1b
Alvaro Avila <Alvaro.Avila1@alu.uclm.es>
2023-12-20 01:21:30 +0100

    eliminados imports innecesarios

--- src/main/java/com/library/Library/entity/Prestamo.java
+++ src/main/java/com/library/Library/entity/Prestamo.java
@@ -2,16 +2,12 @@ package com.library.Library.entity;
 
 import java.util.Date;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
--- src/main/java/com/library/Library/entity/Usuario.java
+++ src/main/java/com/library/Library/entity/Usuario.java
@@ -4,14 +4,9 @@ import java.util.Date;
 import java.util.List;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToMany;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
